7ed89704b58dc7310f72cbeac2231a9550187398
Aadit Gupta <77019332+Nov3mR@users.noreply.github.com>
2025-03-19 18:52:19 +0000

    First Commit

---
+++ helloWorld/helloWorld.c
@@ -0,0 +1,8 @@
+#include <stdio.h> How to print Hello World in C
+
+//Defining the main function
+
+int main(void){
+    //Printing Hello World! with a newline
+    printf("Hello World!\n"); // semicolon to end a line
+}
